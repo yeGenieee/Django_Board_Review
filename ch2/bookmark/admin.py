36e587862f0f92ca657c6673c18c174be1700c66
yeGenieee <cyj95428@gmail.com>
2018-11-22 13:16:26 +0900

    Add Model

--- ch2/bookmark/admin.py
+++ ch2/bookmark/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from bookmark.models import Bookmark
 
 # Register your models here.
+# models.py에서 정의한 테이블을 Admin 사이트에 보이도록 등록해야함
+
+# Bookmark 클래스가 Admin 사이트에서 어떤 모습으로 보여줄지를 정의하는 클래스
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url') # title과 url을 화면에 출력해라
+
+
+admin.site.register(Bookmark, BookmarkAdmin) # admin.site.register() 를 사용해 Bookmark와 BookmarkAdmin 클래스를 등록
